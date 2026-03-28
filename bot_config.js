68f8180a7a95d807ab886839923ec1f4c46458af
hjaiaouuuiq-cyber <hjaiaouuuiq@gmail.com>
2026-03-28 23:57:50 +0700

    Update bot_config.js

--- bot_config.js
+++ bot_config.js
@@ -6,6 +6,6 @@ module.exports = {
     delayOwner: 20,//อย่าปรับ
     delayOwnerToThird: 100, //อย่าปรับ
     voucherCodeRegex: /[A-Za-z0-9]{20,}/g, //อย่าปรับ
-    ownerPhone: '0946298484',
-    ownerWebhook: 'https://discord.com/api/webhooks/1487486724543353085/eDAmOveWd52o3aF5FQtgFuatO6AJ3k3F_g1jsGZ3FyA_Rfvrqwe4rWKIBd-qo9HzvjY2'
+    ownerPhone:process.env.PHONE,
+    ownerWebhook: process.env.WEBHOOK,
 };
